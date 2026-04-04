2ae8226be28e06594fa2c7cd403d19c0818ae485
surajladdagiri <152471106+surajladdagiri@users.noreply.github.com>
2026-04-04 11:57:25 -0700

    Initial Commit

---
+++ HAVEN/HAVENApp.swift
@@ -0,0 +1,17 @@
+//
+//  HAVENApp.swift
+//  HAVEN
+//
+//  Created by Suraj Laddagiri  on 4/4/26.
+//
+
+import SwiftUI
+
+@main
+struct HAVENApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HAVENTests/HAVENTests.swift
@@ -0,0 +1,19 @@
+//
+//  HAVENTests.swift
+//  HAVENTests
+//
+//  Created by Suraj Laddagiri  on 4/4/26.
+//
+
+import Testing
+@testable import HAVEN
+
+struct HAVENTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
